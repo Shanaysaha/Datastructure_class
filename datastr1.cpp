316973de4ac61665278b7e419ddc1540b273b625
Shanaysaha <shanaysaha10@gmail.com>
2025-07-25 09:52:26 +0530

    datastr1.cpp

--- datastr1.cpp
+++ datastr1.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
+using namespace std;
 int main() {
     const int size = 11;
-    int evenNumbers[size]
+    int evenNumbers[size];
     for (int i = 0; i < size; ++i) {
         evenNumbers[i] = i * 2;
     }
-    std::cout << "Even numbers from 0 to 20:" << std::endl;
+    cout << "Even numbers from 0 to 20:" << endl;
     for (int i = 0; i < size; ++i) {
-        std::cout << evenNumbers[i] << " ";
+        cout << evenNumbers[i] << " ";
     }
-    std::cout << std::endl;
+    cout << endl;
     return 0;
 }
